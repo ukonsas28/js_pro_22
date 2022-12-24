7e8f342fc071a8124f6230162b099c3d2dc266fe
Alexey Shirokov <shirokovam@lad24.ru>
2022-12-24 11:55:32 +0300

    state + useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import { CounterComponent } from './CounterComponent';
+import { Input } from './Input';
 
 function App() {
+  const handler = () => {
+    console.log('КНОПКА БЫЛА НАЖАТА');
+  };
+
   return (
     <div className="App">
       <CounterComponent title={'Orange'} />
       <CounterComponent title={'Apple'} count={4} />
       <CounterComponent title={'Lemon'} />
+
+      <button onClick={handler}>PUSH ME</button>
+
+      <Input />
     </div>
   );
 }
