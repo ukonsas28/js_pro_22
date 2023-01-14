897b5b45a84250639e7a9809d1967694f9ef90a3
Alexey Shirokov <shirokovam@lad24.ru>
2023-01-14 10:33:14 +0300

    add node-sass + style header, footer

--- src/Components/Common/Footer/index.tsx
+++ src/Components/Common/Footer/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import style from './Footer.module.scss';
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={style.wrapper}>
+      <span className={style.text}>Footer</span>
+    </footer>
+  );
 };
