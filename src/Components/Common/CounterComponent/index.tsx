79575745dcd9c9e6609f0970ccc9f7fcfec6a079
Alexey Shirokov <shirokovam@lad24.ru>
2023-01-20 15:09:45 +0300

    Counter component restore

---
+++ src/Components/Common/CounterComponent/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import style from './CounterComponent.module.scss';
+
+type CounterPropsType = {
+  title: string;
+  count?: number;
+};
+
+export const CounterComponent = (props: CounterPropsType) => {
+  const { title } = props;
+
+  const [count, setCount] = useState(0);
+
+  const incrementHandler = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrementHandler = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <div className={style.counter_wrapper}>
+      <h1>{title}</h1>
+      <div className={style.counter_group}>
+        <button disabled={count === 0} onClick={decrementHandler}>
+          remove
+        </button>
+        <h2>{count}</h2>
+        <button onClick={incrementHandler}>add</button>
+      </div>
+    </div>
+  );
+};
--- src/Components/MainContent/index.tsx
+++ src/Components/MainContent/index.tsx
@@ -1,3 +1,4 @@
+import { CounterComponent } from 'Components/Common/CounterComponent';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import style from './MainContent.module.scss';
@@ -6,8 +7,13 @@ export const MainContent = () => {
   const navigate = useNavigate();
 
   return (
-    <main className={style.content}>
+    <main className={style.wrapper}>
       <h1>MAIN CONTENT</h1>
+
+      <div className={style.content}>
+        <CounterComponent title="Apple" />
+        <CounterComponent title="Orange" />
+      </div>
 
       <div className={style.btn_group}>
         <button type="button" onClick={() => navigate(-1)}>
