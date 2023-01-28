3a6d59cde3e59be40236a9f118fa890a2fdf0d03
Alexey Shirokov <shirokovam@lad24.ru>
2023-01-28 12:06:34 +0300

    add redirect on main

--- src/Containers/Auth/index.tsx
+++ src/Containers/Auth/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PageWrapper } from 'Components/Common/PageWrapper';
 import { AuthForm } from 'Components/AuthForm';
-import { useSelector } from 'react-redux';
-import { UserSelectors } from 'Store';
+import { useDispatch, useSelector } from 'react-redux';
+import { UserSelectors, UserSliceActions } from 'Store';
+import { useNavigate } from 'react-router';
+import { routes } from 'Helpers/Constants/routes';
 
 export const Auth = () => {
   const loading = useSelector(UserSelectors.getUserLoading);
+  const token = useSelector(UserSelectors.getUserToken);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token && token !== 'error') {
+      navigate(routes.main);
+      dispatch(UserSliceActions.setUserLoading(false));
+    }
+    if (token === 'error') {
+      dispatch(UserSliceActions.setUserLoading(false));
+    }
+  }, [token]);
+
   return <PageWrapper>{loading ? <h1>Loading...</h1> : <AuthForm />}</PageWrapper>;
 };
--- src/Store/User/selectors.ts
+++ src/Store/User/selectors.ts
@@ -3,3 +3,5 @@ import { RootStateType } from '../configureStore';
 export const getUserEmail = (state: RootStateType) => state.userSlice.email;
 
 export const getUserLoading = (state: RootStateType) => state.userSlice.loading;
+
+export const getUserToken = (state: RootStateType) => state.userSlice.token;
